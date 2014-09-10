75069bc07a646ff675215514f7f4192f5bc867d2
Jessica Burns <jburns@neboweb.com>
2014-09-10 13:18:42 -0400

    Added some layout touch ups

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,4 +7,22 @@ class ListsController < ApplicationController
 		@list = List.find(params[:id])
 		@tasks = Task.where listid: params[:id]
 	end
+
+	def create
+		@list = List.new(list_params)
+		if @list.save
+			redirect_to lists_path, flash: {success: "Successfully created list" }
+		else
+			redirect_to new_list_path, flash: {danger: "Could not save list" }
+		end
+	end
+
+	def new
+		@list = List.new
+	end
+
+	private
+    def list_params
+      params.require(:list).permit(:name, :description)
+    end
 end
